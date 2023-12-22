b6b7451c26e84d192ac9c2aa81e164c347b33254
Jabont Chamikorn <Jabont@hotmail.com>
2023-12-22 17:02:52 +0700

    h2

--- index.php
+++ index.php
@@ -5,7 +5,8 @@
 	<meta charset=utf-8>
 	<meta name=description content="">
 	<meta name=viewport content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="dist/jayss.css">
+	<link rel="stylesheet" type="text/css" href="dist/main.css">
+	<link rel="stylesheet" type="text/css" href="dist/jayss-size.css?v=<?=time()?>">
 	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
